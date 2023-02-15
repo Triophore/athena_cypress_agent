8d14d8f2ae8c4d21986192cef2ed8dc9358e27d4
ls <venu@lifesignals.com>
2023-02-15 12:01:09 +0530

    init

--- athena.js
+++ athena.js
@@ -1,2 +1,58 @@
 #!/usr/bin/env node
-console.log("hello from bin")
+var figlet = require('figlet');
+const prompts = require('prompts');
+async function start() {
+
+
+    console.log(figlet.textSync('Athena', {
+        // font: 'Ghost',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true
+    }));
+
+    console.log("Project helper cli")
+
+    const project_id = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Please provide project id',
+        //validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    });
+
+    const r2accountid = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Please provide s3 or R2 Account ID',
+        //validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    });
+
+    const r2accountkeyid = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Please provide s3 or R2 Key ID',
+        //validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    });
+
+    const r2secret = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Please provide s3 or R2 Secret ID',
+        //validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    });
+
+    const agnet_config = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Please paste the agent config json',
+        //validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    });
+
+    console.log(response);
+
+    console.log( process.cwd())
+
+}
+
+start();
