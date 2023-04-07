c7e51c2e87b5693850c206c47195fb86893bc578
ls <venu@lifesignals.com>
2023-04-07 18:25:42 +0530

    added event

--- athena.js
+++ athena.js
@@ -30,7 +30,7 @@ async function start() {
 
         function ThroughDirectory(Directory) {
             fs.readdirSync(Directory).forEach(File => {
-                const Absolute = Path.join(Directory, File);
+                const Absolute = path.join(Directory, File);
                 if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
                 else return Files.push(Absolute);
             });
